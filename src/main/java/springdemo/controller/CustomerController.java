6197f49f8eccca0183ec3a12207f77480df88c9f
Mohamed Ashraf <36118944+mohamedashraf1@users.noreply.github.com>
2022-09-17 22:48:55 +0200

    added the data to the controller

--- src/main/java/springdemo/controller/CustomerController.java
+++ src/main/java/springdemo/controller/CustomerController.java
@@ -1,15 +1,35 @@
 package springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import springdemo.dao.CustomerDAO;
+import springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	// need to inject the customer dao
+	//
+	// spring will scan for a component that
+	// implements CustomerDAO interface
+	//
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+		
+		// get customers from the dao
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
 	}
